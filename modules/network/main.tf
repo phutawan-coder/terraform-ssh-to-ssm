a97056026e2f21b5da5edeca3c8ec6f9dfb683f2
Phutawan <phutawan@Phutawans-MacBook-Air.local>
2026-01-15 16:46:13 +0700

    ssm with vpc endpoint

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,20 +1,10 @@
 resource "aws_vpc" "this" {
   cidr_block           = "10.0.0.0/16" 
   enable_dns_hostnames = true
+  enable_dns_support = true
 
   tags = {
     Name = "vpc-lab5"
-  }
-}
-
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = var.availability_zone
-
-  tags = {
-    Name = "public-sb-lab5"
   }
 }
 
@@ -28,56 +18,12 @@ resource "aws_subnet" "private" {
   }
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.this.id
-
-  tags = {
-    Name = "igw-lab5"
-  }
-}
-
-resource "aws_eip" "eip" {
-  domain = "vpc"
-
-  tags = {
-    Name = "eip-lab5"
-  }
-}
-
-resource "aws_nat_gateway" "ngw" {
-  allocation_id = aws_eip.eip.id
-  subnet_id     = aws_subnet.public.id
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.this.id 
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "public-rt-lab5"
-  }
-}
-
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.ngw.id
-  }
 
   tags = {
     Name = "private-rt-lab5"
   }
-}
-
-resource "aws_route_table_association" "public-as" {
-  route_table_id = aws_route_table.public.id
-  subnet_id      = aws_subnet.public.id
 }
 
 resource "aws_route_table_association" "private-as" {
@@ -89,9 +35,57 @@ resource "aws_security_group" "private" {
   vpc_id = aws_vpc.this.id
 
   egress {
+    from_port = 443  
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ssm-endpoint" {
+  vpc_id = aws_vpc.this.id
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    security_groups = [ aws_security_group.private.id ]
+  }
+
+  egress {
     from_port = 0  
-    to_port = 0
+    to_port = 0 
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id = aws_vpc.this.id
+  vpc_endpoint_type = "Interface"
+  subnet_ids = [ aws_subnet.private.id ]
+  service_name = "com.amazonaws.ap-southeast-2.ssm"
+
+  private_dns_enabled = true
+  security_group_ids = [aws_security_group.ssm-endpoint.id]
+}
+
+resource "aws_vpc_endpoint" "ssmm" {
+  vpc_id = aws_vpc.this.id
+  vpc_endpoint_type = "Interface"
+  subnet_ids = [ aws_subnet.private.id ]
+  service_name = "com.amazonaws.ap-southeast-2.ssmmessages"
+  
+  private_dns_enabled = true
+  security_group_ids = [aws_security_group.ssm-endpoint.id]
+}
+
+resource "aws_vpc_endpoint" "ec2m" {
+  vpc_id = aws_vpc.this.id
+  vpc_endpoint_type = "Interface"
+  subnet_ids = [ aws_subnet.private.id ]
+  service_name = "com.amazonaws.ap-southeast-2.ec2messages"
+
+  private_dns_enabled = true
+  security_group_ids = [aws_security_group.ssm-endpoint.id]
 }
